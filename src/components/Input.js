d11e77cf0357cc7f18e51bf1da6430231e2b8035
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-10-23 12:17:46 -0400

    Clean up unused Input props now that we are using react-hook form

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,26 +1,16 @@
 import React from 'react'
 import { Box, Text, Input as FormInput } from '@chakra-ui/core'
 
-const Input = ({
-  inputId,
-  value,
-  onChange,
-  label,
-  name,
-  refName,
-  error,
-}) => (
+const Input = ({ label, name, refName, error }) => (
   <Box>
-    <Text as="label" fontSize="lg" htmlFor={inputId}>
+    <Text as="label" fontSize="lg" htmlFor={name}>
       {label}
     </Text>
     <FormInput
       id={name}
       name={name}
-      value={value}
       ref={refName}
       size="md"
-      onChange={onChange}
       width="300px"
       marginBottom="1rem"
       isInvalid={error ? true : false}
