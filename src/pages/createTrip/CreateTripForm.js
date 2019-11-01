11e8146ba59febf46730c94473399566ba874503
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-11-01 16:38:49 -0400

    Added some comments for createTripForm function

--- src/pages/createTrip/CreateTripForm.js
+++ src/pages/createTrip/CreateTripForm.js
@@ -17,9 +17,10 @@ const CreateTripForm = ({ setTrip }) => {
     getValues,
   } = useForm()
 
+  // This submit method will only get called if there are no form validation errors thrown
   const onSubmit = data => {
-    const { members } = getValues({ nest: true })
     //TODO: send to DB, get OK response
+    const { members } = getValues({ nest: true })
 
     //then set state if successful
     setTrip({
@@ -31,13 +32,13 @@ const CreateTripForm = ({ setTrip }) => {
     history.push('/trip')
   }
 
+  /* getValues passing nest = true will give output an object where all form elements that use
+     the nested naming convention (member[0], member[1] etc.) are combined and added to an array.
+     Then we can filter it out, and use setValue from react-hook-form to manually set the new values.
+     memberCount determines the number of inputs to display, so they should always be in sync
+  */
   const removeMember = index => {
     const { members } = getValues({ nest: true })
-    /* getValues passing nest = true will give output an object where all form elements that use
-       the nested naming convention (member[0], member[1] etc.) are combined and added to an array.
-       Then we can filter it out, and use setValue from react-hook-form to manually set the new values.
-       memberCount determines the number of inputs to display, so they should always be in sync
-    */
     const newMembers = members.filter((value, i) => i !== index)
     newMembers.map((member, i) => setValue(`members[${i}]`, member))
     setMemberCount(memberCount - 1)
