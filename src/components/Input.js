c1b0fb9768b7d3f6845316573ee78d264bbee6e4
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2019-11-01 15:11:35 -0400

    Refactor Input component to use an InputError component as well

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,34 +1,26 @@
 import React from 'react'
 import { Box, Text, Input as FormInput } from '@chakra-ui/core'
+import InputError from './InputError'
 
 const Input = ({ label, name, refName, error }) => (
   <Box>
-    <Text as="label" fontSize="lg" htmlFor={name}>
-      {label}
-    </Text>
+    {label ? (
+      <Text as="label" fontSize="lg" htmlFor={name}>
+        {label}
+      </Text>
+    ) : null}
     <FormInput
       id={name}
       name={name}
       ref={refName}
       size="md"
-      width="300px"
-      marginBottom="1rem"
+      width="100%"
       isInvalid={error ? true : false}
       errorBorderColor="red.300"
-      mb="0"
+      mb="0.25rem"
       aria-describedby={`${name}-error`}
     />
-    <Text
-      id={`${name}-error`}
-      as="span"
-      fontSize="sm"
-      mb="0.5rem"
-      color="red.600"
-      height="1.5rem"
-      display="block"
-    >
-      {error}
-    </Text>
+    <InputError inputName={name}>{error}</InputError>
   </Box>
 )
 
---
+++ src/components/InputError.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Text, Icon, Stack } from '@chakra-ui/core'
+
+const InputError = ({ children, inputName }) => (
+  <Stack isInline>
+    {children ? <Icon name="warning-2" color="red.600" /> : null}
+    <Text
+      id={`${inputName}-error`}
+      as="span"
+      fontSize="sm"
+      mb="0.5rem"
+      color="red.600"
+      height="1.5rem"
+      display="block"
+    >
+      {children}
+    </Text>
+  </Stack>
+)
+
+export default InputError
