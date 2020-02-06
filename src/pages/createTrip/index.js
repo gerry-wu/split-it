83b862424ef130b2ddeead7c31c4fac24c81872a
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2020-02-06 12:28:42 -0500

    Removed unnecessary props now that CreateTripForm uses hooks/context

--- src/pages/createTrip/index.js
+++ src/pages/createTrip/index.js
@@ -2,24 +2,13 @@ import React, { useState } from 'react'
 import { Flex, Heading } from '@chakra-ui/core'
 import CreateTripForm from './CreateTripForm'
 
-const CreateTripPage = ({ setTrip }) => {
-  const [tripName, setTripName] = useState('')
-  const handleChange = event => setTripName(event.target.value)
-
-  const onSubmit = () =>
-    console.log(`We have submitted the form! Tripname: ${tripName}`)
-
+const CreateTripPage = () => {
   return (
     <Flex direction="column" margin="2rem">
       <Heading as="h1" fontSize="60px" marginBottom="3rem">
         Create a trip
       </Heading>
-      <CreateTripForm
-        tripName={tripName}
-        handleChange={handleChange}
-        onSubmit={onSubmit}
-        setTrip={setTrip}
-      />
+      <CreateTripForm />
     </Flex>
   )
 }
