59a364df5e0f347131d87e14a8b664c3e93ec517
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2020-02-06 12:26:36 -0500

    Improved spacing for inputs

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import { Box, Text, Input as FormInput } from '@chakra-ui/core'
 import InputError from './InputError'
 
 const Input = ({ label, name, refName, error }) => (
-  <Box>
+  <Box mb="0.5rem">
     {label ? (
       <Text as="label" fontSize="lg" htmlFor={name}>
         {label}
@@ -17,7 +17,6 @@ const Input = ({ label, name, refName, error }) => (
       width="100%"
       isInvalid={error ? true : false}
       errorBorderColor="red.300"
-      mb="0.25rem"
       aria-describedby={`${name}-error`}
     />
     <InputError inputName={name}>{error}</InputError>
