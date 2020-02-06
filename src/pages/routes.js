b84b5f6f05fa758344ff6b172dd61f96710e9d2f
gerrywu1990 <gerrywu1990@gmail.com>
2020-02-06 10:51:13 -0500

    fix typo

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import TripPage from './trip'
 import CreateTripPage from './createTrip'
-import LandingPge from './landing'
+import LandingPage from './landing'
 import { UserContext } from '../providers/UserProvider'
 
 const AppRoutes = ({ trip, setTrip }) => {
@@ -12,7 +12,7 @@ const AppRoutes = ({ trip, setTrip }) => {
     <>
       <Route path="/trip/:tripId" component={TripPage} />
       <Route exact path="/">
-        {user ? <Redirect to="/create-trip" /> : <LandingPge />}
+        {user ? <Redirect to="/create-trip" /> : <LandingPage />}
       </Route>
       <Route
         path="/create-trip"
