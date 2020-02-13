c756249a764b07a9d024892c68738e48492cfcd4
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2020-02-13 11:39:58 -0500

    Add aria-label for TripCard

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Box, Heading, Text } from '@chakra-ui/core'
-const TripCard = ({ tripId, title, members, ...rest }) => (
-  <Link to={`/trip/${tripId}`}>
-    <Box p={5} shadow="md" borderWidth="1px" {...rest}>
-      <Heading fontSize="xl">{title}</Heading>
-      <Text mt={2} color="gray.500">
-        {members.join(', ')}
-      </Text>
-    </Box>
-  </Link>
-)
+const TripCard = ({ tripId, title, members, ...rest }) => {
+  const ariaLabelText = `Go to the details page for your trip to ${title}`
+  return (
+    <Link to={`/trip/${tripId}`} aria-label={ariaLabelText}>
+      <Box p={5} shadow="md" borderWidth="1px" {...rest}>
+        <Heading fontSize="xl">{title}</Heading>
+        <Text mt={2} color="gray.500">
+          {members.join(', ')}
+        </Text>
+      </Box>
+    </Link>
+  )
+}
 
 export default TripCard
